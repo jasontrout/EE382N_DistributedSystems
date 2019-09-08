fa367cf7c82d6b952b76a4b7efc5b4c6b9d03535
Jason E. Trout <jsnetrout@gmail.com>
2019-09-07 22:32:48 -0500

    List working on UDP server.

---
+++ README.txt
@@ -0,0 +1,1 @@
+java Server 10000 10001 input/inventory.txt
--- Server.java
+++ Server.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.InterruptedException;
 import java.lang.StringBuilder;
+import java.lang.Thread;
 import java.net.InetAddress;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -12,7 +14,7 @@ import java.util.Scanner;
 
 public class Server {
 
-  static class ProductData {
+  class ProductData {
     private Map<String, Integer> productMap = new TreeMap<>();
 
     public ProductData() {}
@@ -22,8 +24,8 @@ public class Server {
       Scanner sc = new Scanner(file);
       while (sc.hasNextLine()) { 
         String name = sc.next(); 
-	int quantity = sc.nextInt();
-	productMap.put(name, quantity);
+    	  int quantity = sc.nextInt();
+	      productMap.put(name, quantity);
       }
       sc.close();
     }
@@ -35,46 +37,91 @@ public class Server {
     public synchronized String toString() { 
       StringBuilder sb = new StringBuilder();
       for (String name : productMap.keySet()) {
-	int quantity = productMap.get(name);
+	      int quantity = productMap.get(name);
         sb.append(name).append(" ").append(quantity).append("\n");
       }
       return sb.toString();
     }
-
   }
 
-  private static ProductData productData = new ProductData();
+  private int tcpPort;
+  private int udpPort;
+  private String fileName;
 
-  static class UdpServer {
+  private ProductData productData = new ProductData();
+
+  class UdpServerThread extends Thread {
     private DatagramSocket socket;
     private byte[] buf = new byte[1024];
+    private int port;
+    private boolean running = true;
 
-    public UdpServer(int udpPort) throws IOException {
-      socket = new DatagramSocket(udpPort);
+    public UdpServerThread(int port) {
+      this.port = port;
     }
 
     public void run() {
-      while (true) {
-        try { 
-        DatagramPacket packet = new DatagramPacket(buf, buf.length);
-	socket.receive(packet);
+      try {
+        System.out.println("UDP server started. Listening on port " + port);
+        socket = new DatagramSocket(port);
+        while (running) {
+          try { 
+            DatagramPacket srcPacket = new DatagramPacket(buf, buf.length);
+	          socket.receive(srcPacket);
 
-	InetAddress address = packet.getAddress();
-	int port = packet.getPort();
-        String cmd = new String(buf);
-	String[] tokens = cmd.split(" ");
+	          InetAddress srcAddress = srcPacket.getAddress();
+	          int srcPort = srcPacket.getPort();
+            String cmd = new String(srcPacket.getData(), 0, srcPacket.getLength());
+	          String[] tokens = cmd.split(" ");
 
-	if (tokens[0].equals("list")) {
-          byte[] retBytes = productData.toString().getBytes();
-	  DatagramPacket retPacket = new DatagramPacket(retBytes, retBytes.length, address, port);
-	  socket.send(retPacket);
-	}
-      } catch (Exception ex) {
-	    ex.printStackTrace();
-
+	          if (tokens[0].equals("list")) {
+              byte[] dstBytes = productData.toString().getBytes();
+	            DatagramPacket dstPacket = new DatagramPacket(dstBytes, dstBytes.length, srcAddress, srcPort);
+	            socket.send(dstPacket);
+	          }
+          } catch (Exception ex) {
+	          ex.printStackTrace();
+          }
+        }
+      } catch (Exception e) {
+        System.out.println("Error in UdpServerThread.");
+        e.printStackTrace();
       }
-
     }
+
+    @Override
+    public void interrupt() {
+      super.interrupt();
+      running = false;
+      socket.close();
+    }
+  }
+
+  public Server() { }
+
+  public Server(int tcpPort, int udpPort, String fileName) {
+    this.tcpPort = tcpPort;
+    this.udpPort = udpPort;
+    this.fileName = fileName;
+  }
+
+  public void start() {
+
+    try {
+
+      // Load inventory file.
+      productData.loadFromFile(fileName);
+
+      // Start the UDP server.
+      UdpServerThread udpServerThread = new UdpServerThread(udpPort);
+      udpServerThread.start();
+
+      // Wait for the threads to finish.
+      udpServerThread.join();
+    } catch (InterruptedException e) {
+      System.out.println("Main thread interrupted.");
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
 
@@ -89,23 +136,13 @@ public class Server {
 
       System.exit(-1);
     }
+
     tcpPort = Integer.parseInt(args[0]);
     udpPort = Integer.parseInt(args[1]);
     String fileName = args[2];
 
-    // parse the inventory file
-    try {
-      productData.loadFromFile(fileName);
-    } catch (Exception e) { 
-      e.printStackTrace();
-    }
+    Server server = new Server(tcpPort, udpPort, fileName);
+    server.start();
 
-    // TODO: handle request from clients
-    try {
-      UdpServer udpServer = new UdpServer(udpPort);
-      udpServer.run();
-    } catch (Exception e) {  
-      e.printStackTrace();
-    }
   }
 }
